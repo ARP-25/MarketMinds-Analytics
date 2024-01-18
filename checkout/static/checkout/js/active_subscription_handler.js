9f78981a675b58a479245ec7b923531c753970fe
Angelo Rocco Pucci <angelo.pucci@outlook.de>
2024-01-18 12:03:45 +0000

    Improved SEO. Keyword Stuffing in semantic elements, descriptive meta tags, included sitemap.xml and robots.txt files.

--- checkout/static/checkout/js/active_subscription_handler.js
+++ checkout/static/checkout/js/active_subscription_handler.js
@@ -3,23 +3,18 @@ var activeSubscriptionPlan_ids = JSON.parse($('#id_active_subscription_plan').te
 activeSubscriptionPlan_ids.forEach(function(id, index, array) {
     array[index] = parseInt(id, 10);
 });
-
-
 var elementsWithDataItemId = $('div[data-item-id]');
 var newSubscriptionPlan_ids = [];
 elementsWithDataItemId.each(function() {
     var itemId = $(this).data('item-id');
     newSubscriptionPlan_ids.push(itemId);
 });
-
-
 var id_matches = [];
 activeSubscriptionPlan_ids.forEach(function(id) {
     if (newSubscriptionPlan_ids.includes(id)) {
         id_matches.push(id);
     }
 });
-
 
 $('div[data-item-id]').each(function() {
     var itemId = $(this).data('item-id');    
