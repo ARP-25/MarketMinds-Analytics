5a94792dc940cb29abe6ebe06ef68d6c891333cd
Angelo Rocco Pucci <angelo.pucci@outlook.de>
2024-02-29 16:29:37 +0000

    Replaced django summernote with django ck-editor for text fields in Insight model

--- marketminds_analytics/settings.py
+++ marketminds_analytics/settings.py
@@ -86,6 +86,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap4',
     'storages',
+    'ckeditor',
 ]
 
 MIDDLEWARE = [
--- trade_insights/models.py
+++ trade_insights/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django_summernote.fields import SummernoteTextField
+from ckeditor.fields import RichTextField
 from profiles.models import UserProfile  
 from subscription.models import SubscriptionPlan
 
@@ -7,7 +7,7 @@ from subscription.models import SubscriptionPlan
 class Insight(models.Model):
     title = models.CharField(max_length=200)
     release_date = models.DateField()
-    content = SummernoteTextField()
+    content = RichTextField()
     category = models.ForeignKey(SubscriptionPlan, on_delete=models.CASCADE, related_name='insights')
     short_description = models.TextField()
     author = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, null=True, related_name='authored_insights')
@@ -25,6 +25,6 @@ class Insight(models.Model):
         choices=STAGE_CHOICES,
         default=BACKSTAGE,
     )
-    
+
     def __str__(self):
         return self.title
