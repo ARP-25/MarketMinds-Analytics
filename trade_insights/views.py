113508ccfb7c46f0916ecb17c4ab48f5b9c31e7c
Angelo Rocco Pucci <angelo.pucci@outlook.de>
2024-03-02 09:30:07 +0000

    Added products section in trade insight sidebar

--- trade_insights/views.py
+++ trade_insights/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from .models import Insight
+from subscription.models import SubscriptionPlan
 
 # Create your views here.
 def trade_insights(request):
-    # Filtern der Insights für Main Stage und Second Stage
     ms_insights = Insight.objects.filter(stage='MS')
     ss_insights = Insight.objects.filter(stage='SS')
-
-    # Beide Listen in den Kontext einfügen
+    sp = SubscriptionPlan.objects.all()
     context = {
         'ms_insights': ms_insights,
         'ss_insights': ss_insights,
+        'sp': sp,
     }
 
-    # Kontext an das Template übergeben
     return render(request, 'trade_insights/trade_insights.html', context)
