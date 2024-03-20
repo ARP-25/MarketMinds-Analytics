ea3918321ec504e8ae1b1494aa9c4e36de3f16db
Angelo <angelo.pucci@outlook.de>
2024-03-20 17:22:09 +0100

    WIP: Enhancements and Cleanup in Signal Processing.

--- platform_metrics/signals.py
+++ platform_metrics/signals.py
@@ -13,9 +13,13 @@ def update_metrics_on_subscription_update_renew(sender, instance, created, **kwa
     if not created and instance.renewal_date:
         period_start = timezone.now().replace(day=1, hour=0, minute=0, second=0)
         metrics, created = FinancialMetrics.objects.get_or_create(period=period_start)
+
         metrics.renewed_subscriptions += 1
+
         metrics.total_revenue += Decimal(instance.monthly_cost)
+
         metrics.monthly_recurring_revenue += Decimal(instance.monthly_cost)
+
         metrics.save()
 
         print("Financial metrics updated for renewed subscription.")
@@ -24,20 +28,20 @@ def update_metrics_on_subscription_update_renew(sender, instance, created, **kwa
 
 @receiver(post_save, sender=ActiveSubscription)
 def update_metrics_on_subscription_update_cancel(sender, instance, created, **kwargs):
-    
-    # Check if the instance is being updated (not created) and if canceled_at is set
-    if not created and instance.canceled_at:
-        print("We are in the signal update cancel subscription")
+    print("We are in the signal update cancel subscription")
+    if not created and instance.canceled_at:       
         period_start = timezone.now().replace(day=1, hour=0, minute=0, second=0)
         metrics, created = FinancialMetrics.objects.get_or_create(period=period_start)
+
         metrics.canceled_subscriptions += 1
 
-        # Ensuring total_revenue field on FinancialMetrics model is Decimal field
         metrics.total_revenue = Decimal(str(metrics.total_revenue))
         metrics.total_revenue -= Decimal(instance.monthly_cost)
 
-        # Ensuring monthly_recurring_revenue field on FinancialMetrics model is Decimal field
+        
         metrics.monthly_recurring_revenue = Decimal(str(metrics.monthly_recurring_revenue))
         metrics.monthly_recurring_revenue -= Decimal(instance.monthly_cost)
 
         metrics.save()
+        
+        print("Financial metrics updated for cancelled subscription.")
