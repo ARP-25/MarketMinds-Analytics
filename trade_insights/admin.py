e34e288cbfb026f158646f8f5b6ee4bd7d12471d
Angelo <angelo.pucci@outlook.de>
2024-03-21 22:35:51 +0100

    Redesign Frontend of Admin Access Trade Insight Page
    - Revamp the layout and style for improved user experience.
    - Enhance responsiveness and visual appeal of the trade insight section.
    - Implement updated alignment and positioning of elements.

--- trade_insights/admin.py
+++ trade_insights/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Insight
+from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Insight)
 class InsightAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'release_date', 'category')
     search_fields = ('title', 'content')
- 
+    summernote_fields = ('content',)
--- trade_insights/views.py
+++ trade_insights/views.py
@@ -1,8 +1,11 @@
+from copy import deepcopy
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.db.models import Q
 from .models import Insight
 from django.shortcuts import render, get_object_or_404
 from subscription.models import SubscriptionPlan
+from checkout.models import ActiveSubscription
 
 # Create your views here.
 def trade_insights(request):
@@ -18,14 +21,37 @@ def trade_insights(request):
     return render(request, 'trade_insights/trade_insights.html', context)
 
 def trade_insights_detail(request, slug):
-    sp = SubscriptionPlan.objects.filter(staged=True)
+    staged_subscription_plans = SubscriptionPlan.objects.filter(staged=True)
     insight = get_object_or_404(Insight, slug=slug)
     category = insight.category
-    similiar_insights = Insight.objects.filter(category=category)
-    context = {
-        'sp': sp,
-        'insight': insight,
-        'similiar_insights': similiar_insights
-    }
+    similar_insights = Insight.objects.filter(category=category)
+    print(similar_insights)
+    user = request.user
+    if user.is_authenticated and user.active_subscriptions.filter(
+        Q(subscription_plan=insight.category) & 
+        (Q(status='active') | Q(status='pending cancellation'))
+    ).exists():
+        print(f"User is subscribed to {insight.category}")
+        subscribed = True
 
+        context = {
+            'sp': staged_subscription_plans,
+            'subscribed': subscribed,
+            'insight': insight,  
+            'similar_insights': similar_insights
+        }
+    else:
+        print("User is not subscribed or not authenticated")
+        subscribed = False
+        reduced_insight = deepcopy(insight)
+        reduced_insight.content = reduced_insight.content[:300] + ' ...'
+        context = {
+            'sp': staged_subscription_plans,
+            'subscribed': subscribed,
+            'insight': reduced_insight,  
+            'similar_insights': similar_insights,
+            'subscription_plan': category,
+        }
+    
     return render(request, 'trade_insights/trade_insights_detail.html', context)
+        
