499ef372c9a3973aedb8e70a52870d64c2b4f895
Angelo <angelo.pucci@outlook.de>
2024-03-24 21:24:31 +0100

    Switch settings to production

--- marketminds_analytics/settings.py
+++ marketminds_analytics/settings.py
@@ -19,7 +19,7 @@ load_dotenv()
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
